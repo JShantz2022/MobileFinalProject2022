fd170d68f45a5612dd868872c0a8c94753f77647
Josslyne <Jshantz8979@conestogac.on.ca>
2022-04-07 17:30:21 -0400

    Added components and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavComponent } from './nav/nav.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { ViewCustomersComponent } from './view-customers/view-customers.component';
+import { AddDriverComponent } from './add-driver/add-driver.component';
+import { EditDriverComponent } from './edit-driver/edit-driver.component';
+import { ViewDriversComponent } from './view-drivers/view-drivers.component';
+import { AddDeliveryComponent } from './add-delivery/add-delivery.component';
+import { EditDeliveryComponent } from './edit-delivery/edit-delivery.component';
+import { ViewDeliverysComponent } from './view-deliverys/view-deliverys.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    ViewCustomersComponent,
+    AddDriverComponent,
+    EditDriverComponent,
+    ViewDriversComponent,
+    AddDeliveryComponent,
+    EditDeliveryComponent,
+    ViewDeliverysComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/view-deliverys/view-deliverys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-deliverys',
+  templateUrl: './view-deliverys.component.html',
+  styleUrls: ['./view-deliverys.component.css']
+})
+export class ViewDeliverysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
