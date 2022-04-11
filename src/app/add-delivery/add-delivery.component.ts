544a92ffc3227f337a979455700814929175151f
Josslyne <Jshantz8979@conestogac.on.ca>
2022-04-11 16:00:59 -0400

    Revert "Revert "Set up delivery add page - not working""
    This reverts commit 629730012ca7f34263d3f881a3527e342f743e51.

--- src/app/add-delivery/add-delivery.component.ts
+++ src/app/add-delivery/add-delivery.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {Delivery} from "../models/delivery.model";
 
 @Component({
-  selector: 'app-add-delivery',
-  templateUrl: './add-delivery.component.html',
-  styleUrls: ['./add-delivery.component.css']
+    selector: 'app-add-delivery',
+    templateUrl: './add-delivery.component.html',
+    styleUrls: ['./add-delivery.component.css']
 })
 export class AddDeliveryComponent implements OnInit {
+    foods = ["Apples", "Bananas", "Potatoes", "Carrots", "Steak", "Ground Beef"];
+    selectedFoods: string[];
+    customers = [[1,"Shirley"], [2,"Adam"], [3,"Micheal"]];
+    drivers = [[1,"Harry"], [2,"Undine"], [3,"Porsha"]];
+    delivery: Delivery = new Delivery();
+    constructor() { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
-
+    btnSave_click(){
+        this.delivery.setFoods(this.selectedFoods);
+        this.delivery.print();
+    }
 }
